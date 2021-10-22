43631d4fef11f798ffa0b1fc25a4b20a04384b4d
Filip Mistrik <filipmistrik@gmail.com>
2021-10-22 19:40:25 +0100

    Add functionality to fetch and display Memos in MainWindow.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,11 +1,21 @@
 #include "MainWindow.hpp"
+#include "model/Memo.hpp"
+#include "remote/GrpcClient.hpp"
+#include "remote/GrpcClientAdapter.hpp"
+#include "remote/model/ListMemos.hpp"
 #include "ui_MainWindow.h"
+#include <QStandardItem>
+#include <QDateTime>
 
 MainWindow::MainWindow()
     : QMainWindow()
     , ui_(new Ui::MainWindow)
 {
     ui_->setupUi(this);
+    connect(ui_->refreshButton, &QPushButton::pressed, this, &MainWindow::fetchMemos);
+    ui_->memoListView->setModel(new QStandardItemModel);
+    auto selectionModel = ui_->memoListView->selectionModel();
+    connect(selectionModel, &QItemSelectionModel::selectionChanged, this, &MainWindow::processMemoSelection);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +23,49 @@ MainWindow::~MainWindow()
     delete ui_;
 }
 
+void MainWindow::fetchMemos()
+{
+    memo::GrpcClientAdapter client(std::make_unique<memo::GrpcClient>("127.0.0.1", "8000"));
+    memo::remote::ListMemosRequest request;
+    request.uuid = "123-123-123-123-123";
+    auto response = client.listMemos(request);
+
+    if (response.ok())
+    {
+        const auto& responseData = response.body();
+        auto model = dynamic_cast<QStandardItemModel*>(ui_->memoListView->model());
+        model->clear();
+        memos_.clear();
+        for (const auto& memo : responseData.memos())
+        {
+            const auto title = QString::fromStdString(memo->title());
+            memos_[title] = memo;
+            auto item = new QStandardItem(title);
+            model->appendRow(item);
+        }
+    }
+}
+
+void MainWindow::processMemoSelection(const QItemSelection& selected, const QItemSelection& deselected)
+{
+    if (!selected.indexes().empty())
+    {
+        auto index = selected.indexes().front();
+        auto memoTitle = ui_->memoListView->model()->data(index).toString();
+        auto iter = memos_.find(memoTitle);
+        if (iter != memos_.end() && iter.value())
+        {
+            const auto memo = iter.value();
+            displayMemo(*memo);
+        }
+    }
+}
+
+void MainWindow::displayMemo(const memo::model::Memo& memo)
+{
+    ui_->memoTitle->setText(QString::fromStdString(memo.title()));
+    ui_->memoDescription->setText(QString::fromStdString(memo.description()));
+    auto dateTime = QDateTime::fromMSecsSinceEpoch(static_cast<long long>(memo.timestamp()));
+    ui_->memoDate->setText(dateTime.toString("dd MMM yyyy  hh:mm:ss"));
+
+}
--- src/MainWindow.hpp
+++ src/MainWindow.hpp
@@ -1,5 +1,11 @@
 #pragma once
 #include <QMainWindow>
+#include <QMap>
+#include <QItemSelection>
+#include <memory>
+
+
+namespace memo::model { class Memo; }
 
 namespace Ui { class MainWindow; }
 
@@ -10,6 +16,14 @@ public:
     MainWindow();
     ~MainWindow() override;
 
+private slots:
+    void fetchMemos();
+    void processMemoSelection(const QItemSelection& selected, const QItemSelection& deselected);
+
+private:
+    void displayMemo(const memo::model::Memo& memo);
+
 private:
     Ui::MainWindow* ui_;
+    QMap<QString, std::shared_ptr<memo::model::Memo>> memos_;
 };
