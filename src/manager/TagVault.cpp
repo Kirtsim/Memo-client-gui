f1a18a8277a7f2198d720dfe3e9822430008f9ae
Filip Mistrik <filipmistrik@gmail.com>
2021-10-31 17:54:08 +0000

    Create TagVault class for managing Tags.

---
+++ src/manager/TagVault.cpp
@@ -0,0 +1,113 @@
+#include "manager/TagVault.hpp"
+#include "model/Tag.hpp"
+
+namespace memo {
+
+using LockGuard = std::lock_guard<std::mutex>;
+
+namespace {
+    std::shared_ptr<model::Tag> CopyOf(const std::shared_ptr<model::Tag>& original);
+} // namespace
+
+TagVector TagVault::list() const
+{
+    const LockGuard lock(mutex_);
+
+    TagVector result;
+    result.reserve(idToTag_.size());
+    for (const auto& [id, tag] : idToTag_)
+    {
+        result.emplace_back(CopyOf(tag));
+    }
+    return result;
+}
+
+std::shared_ptr<model::Tag> TagVault::find(unsigned long id) const
+{
+    const LockGuard lock(mutex_);
+    auto tag = _find(id);
+    return CopyOf(tag);
+}
+
+std::shared_ptr<model::Tag> TagVault::find(const std::string& name) const
+{
+    const LockGuard lock(mutex_);
+    auto tag = _find(name);
+    return CopyOf(tag);
+}
+
+bool TagVault::add(const std::shared_ptr<model::Tag>& tag)
+{
+    const LockGuard lock(mutex_);
+    if (tag)
+    {
+        const auto tagById = _find(tag->id());
+        const auto tagByName = _find(tag->name());
+        if (tagById == tagByName)
+        {
+            auto copy = CopyOf(tag);
+            idToTag_[tag->id()] = copy;
+            nameToTag_[tag->name()] = copy;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool TagVault::remove(unsigned long id)
+{
+    const LockGuard lock(mutex_);
+    if (auto tag = _find(id))
+    {
+        _remove(*tag);
+        return true;
+    }
+    return false;
+}
+
+bool TagVault::remove(const std::string& name)
+{
+    const LockGuard lock(mutex_);
+    if (auto tag = _find(name))
+    {
+        _remove(*tag);
+        return true;
+    }
+    return false;
+}
+
+void TagVault::clear()
+{
+    const LockGuard lock(mutex_);
+    idToTag_.clear();
+    nameToTag_.clear();
+}
+
+void TagVault::_remove(const model::Tag& tag)
+{
+    idToTag_.erase(tag.id());
+    nameToTag_.erase(tag.name());
+}
+
+std::shared_ptr<model::Tag> TagVault::_find(unsigned long id) const
+{
+    auto iter = idToTag_.find(id);
+    return iter != std::end(idToTag_) ? iter->second : nullptr;
+}
+
+std::shared_ptr<model::Tag> TagVault::_find(const std::string& name) const
+{
+    auto iter = nameToTag_.find(name);
+    return iter != std::end(nameToTag_) ? iter->second : nullptr;
+}
+
+namespace {
+    std::shared_ptr<model::Tag> CopyOf(const std::shared_ptr<model::Tag>& original)
+    {
+        if (original)
+            return std::make_shared<model::Tag>(*original);
+        return nullptr;
+    }
+} // namespace
+
+} // namespace memo
---
+++ src/manager/TagVault.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#include <string>
+#include <map>
+#include <mutex>
+#include <vector>
+#include <memory>
+
+namespace memo {
+namespace model { class Tag; }
+
+using TagVector = std::vector<std::shared_ptr<model::Tag>>;
+
+class TagVault
+{
+public:
+    /// @brief Lists all containing tags in a vector. This function returns copies of the tags it holds.
+    /// @return A vector of Memos.
+    TagVector list() const;
+
+    /// @brief Finds a tag with the given id.
+    /// @param id The tag id.
+    /// @return A copy of the found tag or nullptr if tag with the given id was not found.
+    std::shared_ptr<model::Tag> find(unsigned long id) const;
+
+    /// @brief Finds a tag with the given name.
+    /// @param name The tag name.
+    /// @return A copy of the found tag or nullptr if tag with the given name was not found.
+    std::shared_ptr<model::Tag> find(const std::string& name) const;
+
+    /// @brief Adds a copy of the given tag. Succeeds only if the Tag's id and name are unique across all tags.
+    /// The method may as well be used to update existing Tags, provided that the id and name remain unchanged.
+    /// Do not use this method to update Tag's id or name as it won't work. In such cases, remove the old Tag first
+    /// and only then use this method to add it's updated version.
+    /// @param tag The tag to be added to the collection.
+    /// @return True if the tag was added/updated, false otherwise.
+    bool add(const std::shared_ptr<model::Tag>& tag);
+
+    /// @brief Removes tag that has the given id if such tag exists.
+    /// @param id Tag id.
+    /// @return True if the tag was removed, false if not.
+    bool remove(unsigned long id);
+
+    /// @brief Removes tag that has the given name if such tag exists.
+    /// @param name Tag name.
+    /// @return True if the tag was removed, false if not.
+    bool remove(const std::string& name);
+
+    /// @brief Removes all tags. After this operation TagVault will be empty.
+    void clear();
+
+private:
+    void _remove(const model::Tag& tag);
+
+    std::shared_ptr<model::Tag> _find(unsigned long id) const;
+
+    std::shared_ptr<model::Tag> _find(const std::string& name) const;
+
+private:
+    std::map<unsigned long, std::shared_ptr<model::Tag>> idToTag_;
+    std::map<std::string, std::shared_ptr<model::Tag>> nameToTag_;
+    mutable std::mutex mutex_;
+};
+
+} // namespace memo
